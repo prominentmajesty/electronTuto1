a75d5c0f387ef26efe4740ab9dead96ee41a624e
MAJESTY <ugochukwuaugustine7@gmail.com>
2021-03-19 14:42:24 -0700

    A new Commit

---
+++ index.js
@@ -0,0 +1,89 @@
+const electron = require('electron');
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+let mainWindow; 
+let addWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    }); 
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
+    mainWindow.on('close', () => {
+        app.quit();
+    });
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+function createAddWindow(){
+    addWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+        width : 300, 
+        height : 200,
+        title : 'Add New Todo'
+    });  
+
+    addWindow.loadURL(`file://${__dirname}/add.html`);
+}
+
+ipcMain.on('todo:add', (event, todo) => {
+    mainWindow.webContents.send('todo:add', todo);
+    addWindow.close();
+    addWindow = null;
+});
+
+
+const menuTemplate = [
+ 
+    {
+        label : 'File',
+        submenu : [
+            {label: 'New Todo',
+            click(){
+                createAddWindow();
+            }
+        },
+            {
+            label : 'Quit',
+            accelerator : process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+            click(){ 
+                app.quit()
+            } 
+        },
+        {
+        label : 'Delete todo',
+        click(){
+                mainWindow.webContents.send('todo:clear')
+            }
+        } 
+        ]
+    },
+] 
+
+if(process.platform === 'darwin'){
+    menuTemplate.unshift({});
+} 
+if(process.env.NODE_ENV !== 'production'){
+    menuTemplate.push({
+        label: 'View',
+        submenu:[
+            {
+                role : 'reload'
+            },
+            {
+                label : 'Toggle Developer Toolls',
+                accelerator : process.platform === 'darwin' ? 'Command+Alt+I':'Ctrl+Shift+I',
+                click(item,focusedWindow){
+                    focusedWindow.toggleDevTools();
+            }
+        }]
+    })
+}
